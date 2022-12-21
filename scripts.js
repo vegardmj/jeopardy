232125952592b5c4cb1c089b5b4e57caafb6ade8
vegardmj <vegard.aas.mjelde@gmail.com>
2022-12-21 19:28:50 +0100

    asdasd

--- scripts.js
+++ scripts.js
@@ -89,13 +89,18 @@ function addTeam(){
     });
     
     let div = document.getElementById("teams-container");
+    let team = document.createElement("div");
+    team.classList.add("team-wrapper");
+    let scoreCard = document.createElement("div");
+    scoreCard.classList.add("score-card");
     let temp = document.createElement("div");
     temp.classList.add("team")
     temp.setAttribute("id", "team-id-" + value);
     let teamNameElement = document.createElement("h3");
     teamNameElement.innerText = value;
     temp.appendChild(teamNameElement);
-    div.appendChild(temp);
+    team.appendChild(temp);
+    div.appendChild(team);
     
 }
 function removeTeam(){
