7075b7349c5acfb81e3ea21ef65280e4130cd409
vegardmj <vegard.aas.mjelde@gmail.com>
2022-12-21 17:18:33 +0100

    asda

--- scripts.js
+++ scripts.js
@@ -1,4 +1,30 @@
 
+
+let scoreValues = [100, 200, 300, 400, 500];
+
+function getDummyTable(){
+    let table = [];
+    for(let i = 1; i<6; i++){
+        table.push(["Category " + String.valueOf(i), ...scoreValues]);
+    }
+    return table;
+}
+
+function constructGrid(table){
+
+}
+
+
+
+function test1(){
+    constructGrid(getDummyTable());
+}
+
+
+
+
+
+
 function cardClicked(event){
     console.log(event.target);
 }
